8830fa447bef8f1557c13cc8008538439ecd26d3
JanjaMIC <janjakrizman@icloud.com>
2016-03-17 22:35:14 +0100

    pripravljen

---
+++ koda.py
@@ -0,0 +1,43 @@
+spremenljivka = open("dnk.txt").read()
+
+crna = "CCAGCAATCGC"
+rjava =  "GCCAGTGCCG"
+korencek = "TTAGCTATCGC"
+
+
+kvadraten = "GCCACGG"
+okrogel = "ACCACAA"
+ovalen = "AGGCCTCA"
+
+rjava = "AAGTAGTGAC"
+
+modra = "TTGTGGTGGC"
+zelena = "GGGAGGTGGC"
+
+moski = "TGCAGGAACTTC"
+zenska = "TGAAGGACCTTC"
+
+
+belec = "AAAACCTCA"
+crnec = "CGACTACAG"
+azijec = "CGCGGGCCG"
+
+print "crna: ", spremenljivka.find("CCAGCAATCGC")
+print "rjava: ", spremenljivka.find("GCCAGTGCCG")
+print "korencek: ", spremenljivka.find("TTAGCTATCGC")
+print "kvadraten: ", spremenljivka.find("GCCACGG")
+print "okrogel: ", spremenljivka.find("ACCACAA")
+
+print "ovalen: ", spremenljivka.find("AGGCCTCA")
+print "modra: ", spremenljivka.find("TTGTGGTGGC")
+print "zelena: ", spremenljivka.find("GGGAGGTGGC")
+
+print "rjava: ", spremenljivka.find("AAGTAGTGAC")
+
+print "moski: ", spremenljivka.find("TGCAGGAACTTC")
+print "zenska: ", spremenljivka.find("TGAAGGACCTTC")
+
+print "belec: ", spremenljivka.find("AAAACCTCA")
+print "crnec: ", spremenljivka.find("CGACTACAG")
+print "azijec: ", spremenljivka.find("CGCGGGCCG")
+
